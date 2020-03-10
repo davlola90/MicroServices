082ec7720b35fbabe9a27c2ef502c0e1ed6761e5
Aymen Toukabri <aymen.toukabri@corilus.be>
2020-03-10 17:46:26 +0100

    Push

--- MicroRabbit/MicroRabbit.Infra.IOC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -8,6 +8,13 @@ using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Application.Services;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Data.Repository;
+using MicroRabbit.Transfer.Domain.EventHandlers;
+using MicroRabbit.Transfer.Domain.Events;
+using MicroRabbit.Transfer.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -26,20 +33,29 @@ namespace MicroRabbit.Infra.IOC
 
             //Domain Banking Commands
 
+
             services.AddTransient<IRequestHandler<CreateTransferCommand,bool>,TransferCommandhandler>();
+
+
+            //Domain Events 
+            services.AddTransient<IEventhandler<TransferCreatedEvent>, TransferEventHandler>();
 
             //Application Services 
 
             services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<ITransferService, TransferService>();
 
 
             //Data
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<BankingDbContext>();
 
+            services.AddTransient<ITransferRepository, TransferRepository>();
+            services.AddTransient<TransferDbContext>();
 
 
-           
+
+
         }
     }
 }
